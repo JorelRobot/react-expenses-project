1cbbfbd7ef28308b90a8242c03a6d21f7b9bb2a1
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-08-23 14:15:50 -0500

    PRACTICE. Working with Events and State

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,42 +1,37 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
-import React from "react";
+import ExpensesFilter from './ExpensesFilter';
+import './ExpensesFilter.css';
+import React, { useState } from 'react';
 
 const Expenses = (props) => {
-    //*/
+    const [filteredYear, setFilteredYear] = useState('2020');
+
     let expense_items = [];
 
     props.data.forEach(expenseItem => {
         expense_items.push(
-            <ExpenseItem 
+            <ExpenseItem
                 key={expenseItem.title}
-                title={expenseItem.title} 
-                amount={expenseItem.amount} 
+                title={expenseItem.title}
+                amount={expenseItem.amount}
                 date={expenseItem.date}>
             </ExpenseItem>);
     });
-    //*/
 
-    /*/
-    let expense_items = props.data.map(expenseItem => {
-        <ExpenseItem 
-            title={expenseItem.title} 
-            amount={expenseItem.amount} 
-            date={expenseItem.date}>
-        </ExpenseItem>
-    });
-    //*/
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
 
-    return React.createElement(Card, {className: "expenses"}, expense_items);
-
-    /*/
     return (
-        <Card className="expenses">
-            { expense_items }
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {expense_items}
+            </Card>
+        </div>
     );
-    //*/
 }
 
 export default Expenses;
